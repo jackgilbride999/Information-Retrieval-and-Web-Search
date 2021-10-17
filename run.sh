10cd25cb2a4d4c8a62cf697d4ce954f8f2dec255
jackgilbride999 <jackgilbride999@gmail.com>
2021-10-17 20:42:31 +0100

    update bash script

--- run.sh
+++ run.sh
@@ -2,31 +2,31 @@ mvn -q package
 
 echo "Standard Analyzer, VSM Scoring"
 java -jar target/assignment1-1.0.jar cran/cran.all.1400 cran/cran.qry standard vsm
-trec_eval/trec_eval -m map ./cran/QRelsCorrectedforTRECeval ./results.txt 
+trec_eval/trec_eval -m map -m P.5 ./cran/QRelsCorrectedforTRECeval ./results.txt 
 echo ""
 
 echo "Standard Analyzer, BM25 Scoring"
 java -jar target/assignment1-1.0.jar cran/cran.all.1400 cran/cran.qry standard bm25
-trec_eval/trec_eval -m map ./cran/QRelsCorrectedforTRECeval ./results.txt 
+trec_eval/trec_eval -m map -m P.5 ./cran/QRelsCorrectedforTRECeval ./results.txt 
 echo ""
 
 echo "English Analyzer, VSM Scoring"
 java -jar target/assignment1-1.0.jar cran/cran.all.1400 cran/cran.qry english vsm
-trec_eval/trec_eval -m map ./cran/QRelsCorrectedforTRECeval ./results.txt 
+trec_eval/trec_eval -m map -m P.5 ./cran/QRelsCorrectedforTRECeval ./results.txt 
 echo ""
 
 echo "English Analyzer, BM25 Scoring"
 java -jar target/assignment1-1.0.jar cran/cran.all.1400 cran/cran.qry english bm25
-trec_eval/trec_eval -m map ./cran/QRelsCorrectedforTRECeval ./results.txt 
+trec_eval/trec_eval -m map -m P.5 ./cran/QRelsCorrectedforTRECeval ./results.txt 
 echo ""
 
 echo "Custom Analyzer, VSM Scoring"
 java -jar target/assignment1-1.0.jar cran/cran.all.1400 cran/cran.qry custom vsm
-trec_eval/trec_eval -m map ./cran/QRelsCorrectedforTRECeval ./results.txt 
+trec_eval/trec_eval -m map -m P.5 ./cran/QRelsCorrectedforTRECeval ./results.txt 
 echo ""
 
 echo "Custom Analyzer, BM25 Scoring"
 java -jar target/assignment1-1.0.jar cran/cran.all.1400 cran/cran.qry custom bm25
-trec_eval/trec_eval -m map ./cran/QRelsCorrectedforTRECeval ./results.txt 
+trec_eval/trec_eval -m map -m P.5 ./cran/QRelsCorrectedforTRECeval ./results.txt 
 echo ""
 
